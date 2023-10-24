5689cbe7e424137c8a50035b069844d63376d3e7
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:59:48 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_macro_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Macro Calculator by Calculator.iO";
 
 function display_ci_macro_calculator(){
